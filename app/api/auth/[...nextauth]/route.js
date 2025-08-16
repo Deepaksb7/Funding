53465313ea3579efe3b19d617eb76bf3dfe4ceab
Deepaksb7 <beharadeepak6@gmail.com>
2025-08-16 12:03:47 +0530

    trouble with database

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,6 +4,9 @@ import NextAuth from 'next-auth'
 // import GoogleProvider from 'next-auth/providers/google'
 // import EmailProvider from 'next-auth/providers/email'
 import GithubProvider from "next-auth/providers/github"
+import mongoose from 'mongoose'
+import User from '@/models/User'
+import Payment from '@/models/Payment'
 
 
 export const authOptions = NextAuth({
@@ -28,7 +31,27 @@ export const authOptions = NextAuth({
     //   server: process.env.MAIL_SERVER,
     //   from: 'NextAuth.js <no-reply@example.com>'
     // }),
-  ]
+  ],callbacks: {
+  async signIn({ user, account, profile, email, credentials }) {
+    if (account.provider == "github"){
+      const client = await mongoose.connect("mongodb://localhost:27017/funds")
+      const currentUser = User.findOne({email:email})
+      if (!currentUser){
+        const newUser = new User({
+          email:email,
+          username:email.split("@")[0],
+        })
+        await newUser.save()
+        user.name = newUser.username
+      }
+      else{
+        user.name = currentUser.username
+      }
+      return true
+    }
+    
+  }
+}
 })
 
 export {authOptions as GET, authOptions as POST} 
--- components/Dashboard.js
+++ components/Dashboard.js
@@ -27,42 +27,37 @@ const Dashboard = () => {
 
                 <div className='my-2'>
                     <label htmlFor="name" className='mb-2 text-sm font-bold' >Name</label>
-                    <input value={form.name ? form.name : ""} onChange={handlechange} type="text" placeholder-white placeholder='Enter Your Name' name='name' className='w-full p-2 rounded-lg text-gray-900 border border-white' />
+                    <input value={form.name ? form.name : ""} onChange={handlechange} type="text"  placeholder='Enter Your Name' name='name' className='w-full placeholder-white mt-1 bg-slate-800 p-2 rounded-lg  text-gray-900 border border-white' />
                 </div>
 
                 <div className='my-2'>
                     <label htmlFor="email" className='mb-2 text-sm font-bold' >Email</label>
-                    <input value={form.name ? form.name : ""} onChange={handlechange} type="text" placeholder-white placeholder='Enter Your Name' name='name' className='w-full p-2 rounded-lg text-gray-900 border border-white' />
+                    <input value={form.name ? form.name : ""} onChange={handlechange} type="text"  placeholder='Enter Your Email' name='email' className='w-full mt-1 placeholder-white bg-slate-800 p-2 rounded-lg  text-gray-900 border border-white' />
                 </div>
 
                 <div className='my-2'>
-                    <label htmlFor="email" className='mb-2 text-sm font-bold' >Username</label>
-                    <input value={form.name ? form.name : ""} onChange={handlechange} type="text" placeholder-white placeholder='Enter Your Username' name='name' className='w-full p-2 rounded-lg text-gray-900 border border-white' />
+                    <label htmlFor="Username" className='mb-2 text-sm font-bold' >Username</label> placeholder-white
+                    <input value={form.name ? form.name : ""} onChange={handlechange} type="text"  placeholder='Enter Your Username' name='Username' className='w-full placeholder-white bg-slate-800 mt-1 p-2 rounded-lg  text-gray-900 border border-white' />
                 </div>
 
                 <div className='my-2'>
-                    <label htmlFor="name" className='mb-2 text-sm font-bold' >Profile pic</label>
-                    <input value={form.name ? form.name : ""} onChange={handlechange} type="text" placeholder='Profile Pic' name='name' className='w-full p-2 rounded-lg text-gray-900 border border-white' />
+                    <label htmlFor="Profile picture" className='mb-2 text-sm font-bold' >Profile picture</label>
+                    <input value={form.name ? form.name : ""} onChange={handlechange} type="text" placeholder='Profile picture' name='Profilepicture' className='w-full placeholder-white bg-slate-800 p-2 rounded-lg mt-1 text-gray-900 border  border-white' />
                 </div>
 
                 <div className='my-2'>
-                    <label htmlFor="name" className='mb-2 text-sm font-bold' >Cover pic</label>
-                    <input value={form.name ? form.name : ""} onChange={handlechange} type="text" placeholder='Cover Pic' name='name' className='w-full p-2 rounded-lg text-gray-900 border border-white' />
+                    <label htmlFor="Cover picture" className='mb-2 text-sm font-bold' >Cover picture</label>
+                    <input value={form.name ? form.name : ""} onChange={handlechange} type="text" placeholder='Cover picture' name='Coverpicture' className='w-full p-2 placeholder-white bg-slate-800 mt-1 rounded-lg text-gray-900 border  border-white' />
                 </div>
 
                 <div className='my-2'>
-                    <label htmlFor="name" className='mb-2 text-sm font-bold' >Razorpay Id</label>
-                    <input value={form.name ? form.name : ""} onChange={handlechange} type="text" placeholder-white placeholder='Razorpay Id' name='name' className='w-full p-2 rounded-lg text-gray-900 border border-white' />
-                </div>
-
-                <div className='my-2'>
-                    <label htmlFor="name" className='mb-2 text-sm font-bold' >Razorpay Credientials</label>
-                    <input value={form.name ? form.name : ""} onChange={handlechange} type="text" placeholder-white placeholder='Razorpay Credientials' name='name' className='w-full p-2 rounded-lg text-gray-900 border border-white' />
+                    <label htmlFor="Razorpay Id" className='mb-2 text-sm font-bold' >Razorpay Id</label>
+                    <input value={form.name ? form.name : ""} onChange={handlechange} type="text" placeholder='Razorpay Id' name='RazorpayId' className='w-full p-2 placeholder-white rounded-lg mt-1 bg-slate-800 text-gray-900 border  border-white' />
                 </div>
                 
                 <div className='my-2'>
-                    <label htmlFor="name" className='mb-2 text-sm font-bold' >Razorpay Secret</label>
-                    <input value={form.name ? form.name : ""} onChange={handlechange} type="text" placeholder-white placeholder='Razorpay Secret' name='name' className='w-full p-2 rounded-lg text-gray-900 border border-white' />
+                    <label htmlFor="Razorpay Secret" className='mb-2 text-sm font-bold' >Razorpay Secret</label>
+                    <input value={form.name ? form.name : ""} onChange={handlechange} type="text" placeholder='Razorpay Secret' name='RazorpaySecret' className='mt-1 placeholder-white bg-slate-800 w-full p-2 rounded-lg text-gray-900  border border-white' />
                 </div>
 
                 <div className='my-6'>
