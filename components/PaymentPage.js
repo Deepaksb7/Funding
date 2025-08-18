6487d0085a0c8a2f624f63f5aabd2fd9a1a8cf72
Deepaksb7 <beharadeepak6@gmail.com>
2025-08-19 01:34:35 +0530

    payment form almost completed

--- components/PaymentPage.js
+++ components/PaymentPage.js
@@ -1,30 +1,51 @@
 "use client"
-import React,{useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import Script from 'next/script'
-import { initiate } from '@/actions/useractions'
+import { fetchpayment, initiate } from '@/actions/useractions'
 import { useSession } from 'next-auth/react'
+import { fetchuser } from '@/actions/useractions'
 
 const PaymentPage = ({ username }) => {
     // const {data:session} = useSession()
 
-    const [paymentform, setpaymentform] = useState({name:"",message:'',amount:''})
+    const [paymentform, setpaymentform] = useState({ name: '', message: '', amount: '' })
+    const [currentUser, setcurrentUser] = useState({})
+    const [payments, setpayments] = useState([])
 
-    const handleChange=(e)=>{
-        setpaymentform({...paymentform,[e.target.name]:e.target.value})
+    useEffect(() => {
+        getData()
+    }, [])
+
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setpaymentform({
+            ...paymentform,
+            [name]: name === "amount" ? Number(value) : value
+        });
+    };
+
+
+    const getData = async () => {
+        let u = await fetchuser(username)
+        setcurrentUser(u)
+        let dbpayments = await fetchpayment(username)
+        setpayments(dbpayments)
+        console.log(u, dbpayments)
     }
 
     const pay = async (amount) => {
-        let a = await initiate(amount, username,paymentform)
+        let a = await initiate(amount, username, paymentform)
         let orderId = a.id
         var options = {
-            "key": "rzp_test_R5Vs2myBFVaE0w", // Enter the Key ID generated from the Dashboard
+            "key": process.env.NEXT_PUBLIC_KEY_ID, // Enter the Key ID generated from the Dashboard
             "amount": amount, // Amount is in currency subunits. 
             "currency": "INR",
             "name": "Oh ye funds", //your business name
             "description": "Test Transaction",
             "image": "https://example.com/your_logo",
             "order_id": orderId, // This is a sample Order ID. Pass the `id` obtained in the response of Step 1
-            "callback_url": `${process.env.URL}/api/razorpay`,
+            "callback_url": `${process.env.NEXT_PUBLIC_URL}/api/razorpay`,
             "prefill": { //We recommend using the prefill parameter to auto-fill customer's contact information especially their phone number
                 "name": "Gaurav Kumar", //your customer's name
                 "email": "gaurav.kumar@example.com",
@@ -70,42 +91,30 @@ const PaymentPage = ({ username }) => {
                     <div className="supporters w-1/2 bg-slate-900 my-5 text-white p-10">
                         <h2 className='text-2xl font-bold'>Supporter's</h2>
                         <ul className='mx-5 text-lg '>
-                            <li className='my-4 flex items-center gap-2'><img width={28} src="/avatar.gif" alt="" />
-                                <span>
-                                    ram donated <b>$30</b> with a message "I support you bro lot's of love"
-                                </span>
-                            </li>
-                            <li className='my-4 flex items-center gap-2'><img width={28} src="/avatar.gif" alt="" />
-                                <span>
-                                    ram donated <b>$30</b> with a message "I support you bro lot's of love"
-                                </span>
-                            </li>
-                            <li className='my-4 flex items-center gap-2'><img width={28} src="/avatar.gif" alt="" />
-                                <span>
-                                    ram donated <b>$30</b> with a message "I support you bro lot's of love"
-                                </span>
-                            </li>
-                            <li className='my-4 flex items-center gap-2'><img width={28} src="/avatar.gif" alt="" />
-                                <span>
-                                    ram donated <b>$30</b> with a message "I support you bro lot's of love"
-                                </span>
-                            </li>
+                            {payments.map((p, i) => {
+                                return <li key={i} className='my-4 flex items-center gap-2'><img width={28} src="/avatar.gif" alt="" />
+                                    <span>
+                                        {p.name} donated <b>₹{p.amount}</b> with a message "{p.message}"
+                                    </span>
+                                </li>
+                            })}
+
                         </ul>
                     </div>
                     <div className="makepayement w-1/2 bg-slate-900 my-5 text-white p-10">
                         <h2 className='text-2xl font-bold'>Make A Payment</h2>
                         <div className='flex gap-2 flex-col'>
                             <div>
-                                <input onChange={handleChange} value ={paymentform.name} name='name' type="text" className='w-full p-3 rounded-lg bg-slate-800' placeholder='Enter Name' />
+                                <input onChange={handleChange} value={paymentform.name} name='name' type="text" className='w-full p-3 rounded-lg bg-slate-800' placeholder='Enter Name' />
                             </div>
-                            <input onChange={handleChange} value ={paymentform.message} name='message' type="text" className='w-full p-3 rounded-lg bg-slate-800' placeholder='Enter Message' />
-                            <input onChange={handleChange} value ={paymentform.amount} name='amount' type="text" className='w-full p-3 rounded-lg bg-slate-800' placeholder='Enter Amount' />
-                            <button className='xt-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:outline-none font-medium cursor-pointer rounded-lg text-sm px-5 py-2.5 text-center me-2 w-full'>Pay</button>
+                            <input onChange={handleChange} value={paymentform.message} name='message' type="text" className='w-full p-3 rounded-lg bg-slate-800' placeholder='Enter Message' />
+                            <input onChange={handleChange} value={paymentform.amount} name='amount' type="text" className='w-full p-3 rounded-lg bg-slate-800' placeholder='Enter Amount' />
+                            <button onClick={() => pay(paymentform.amount)} className='xt-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:outline-none font-medium cursor-pointer rounded-lg text-sm px-5 py-2.5 text-center me-2 w-full'>Pay</button>
                         </div>
                         <div className='flex gap-2 mt-5'>
-                            <button className='bg-slate-800 p-3 bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:outline-none font-medium cursor-pointer rounded-lg text-sm px-5 text-center'onClick={()=>pay(10)}>Pay ₹10</button>
-                            <button className='bg-slate-800 p-3 bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:outline-none font-medium cursor-pointer rounded-lg text-sm px-5 text-center'onClick={()=>pay(20)}>Pay ₹20</button>
-                            <button className='bg-slate-800 p-3 bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:outline-none font-medium cursor-pointer rounded-lg text-sm px-5 text-center'onClick={()=>pay(30)}>Pay ₹30</button>
+                            <button className='bg-slate-800 p-3 bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:outline-none font-medium cursor-pointer rounded-lg text-sm px-5 text-center' onClick={() => pay(10)}>Pay ₹10</button>
+                            <button className='bg-slate-800 p-3 bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:outline-none font-medium cursor-pointer rounded-lg text-sm px-5 text-center' onClick={() => pay(20)}>Pay ₹20</button>
+                            <button className='bg-slate-800 p-3 bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:outline-none font-medium cursor-pointer rounded-lg text-sm px-5 text-center' onClick={() => pay(30)}>Pay ₹30</button>
                         </div>
 
                     </div>
