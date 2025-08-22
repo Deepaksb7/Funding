857d6ecd5f2aee0b25ee34e4693770d9232da6ae
Deepaksb7 <beharadeepak6@gmail.com>
2025-08-22 23:53:43 +0530

    mongo env added

--- db/connectDB.js
+++ db/connectDB.js
@@ -2,8 +2,9 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(`mongodb+srv://beharadeepak6:stF0iw02VFiVXZVs@cluster0.njhxfsb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    
+    console.log(`MongoDB Connected: {conn.connection.host}`);
   } catch (error) {
     console.error(error.message);
     process.exit(1);
